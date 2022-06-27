623fc40f213c93d08061950bbe1daea5e39e6910
Pius Muriithi <piusmuriithi1@gmail.com>
2022-06-27 21:49:27 +0300

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>I am the Shop</h1>
+    </div>
+  );
+};
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "../assets/imgs/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "../assets/imgs/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "../assets/imgs/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "../assets/imgs/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "../assets/imgs/men.png",
-    },
-  ];
-
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
