594ca4818064c0937433c21a4b8c7d4bfed2d5b4
Pius Muriithi <piusmuriithi1@gmail.com>
2022-07-10 11:02:04 +0300

    Added shopping cart dropdown and cart context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -11,7 +11,7 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
-export const UserPrivider = ({ children }) => {
+export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const value = { currentUser, setCurrentUser };
 
